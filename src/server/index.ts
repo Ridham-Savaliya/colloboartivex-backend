9f41306ace05818225e5edcbc36c9bacbbdbb2a4
Ridham Savaliya <159106564+Ridham-Savaliya@users.noreply.github.com>
2025-07-30 17:11:28 +0530

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -115,7 +115,7 @@ setInterval(() => {
   fetch('https://collaborativex-api.onrender.com/health')
     .then(() => console.log('🔁 Self-ping successful'))
     .catch(() => console.log('❌ Self-ping failed'));
-}, 15 * 60 * 1000); // every 5 minutes
+}, 16 * 60 * 1000); // every 5 minutes
 
 // Fallback route
 app.use((req, res) => {
