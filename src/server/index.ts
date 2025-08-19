70a1d7ab618ceacb76617cb56f1d779884cea447
Ridham-Savaliya <savaliyaridham46@.email.com>
2025-08-19 20:03:22 +0530

    FEAT:health check self-pin added!

--- src/server/index.ts
+++ src/server/index.ts
@@ -111,11 +111,17 @@ app.get('/health', (_, res) => {
   res.send('OK this is regular health check api route to warm out server on render to protect it from cold start❄️😅');
 });
 
-setInterval(() => {
-  fetch('https://collaborativex-api.onrender.com/health')
-    .then(() => console.log('🔁 Self-ping successful'))
-    .catch(() => console.log('❌ Self-ping failed'));
-}, 10 * 60 * 1000); // every 5 minutes
+ setInterval(async () => {
+      try {
+        const res:any = await fetch(`https://collaborativex-whiteboard.vercel.app/api/healthcheck`);
+        const data = await res.json()
+        console.log('main-app health-check✅:',data);
+        
+      } catch (err) {
+        console.error("Keep-alive failed:", err);
+      }
+    }, 10 * 60 * 1000); // 10 minutes
+
 
 // Fallback route
 app.use((req, res) => {
